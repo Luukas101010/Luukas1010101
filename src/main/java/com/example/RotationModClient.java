d6fd5bbbe0c7e7dc3820c9591c13b6f16c416488
Luukas101010 <137522599+Luukas101010@users.noreply.github.com>
2026-03-04 19:49:40 +0200

    Move RotationModClient.java to src/main/java/com/example/

--- src/main/java/com/example/RotationModClient.java
+++ src/main/java/com/example/RotationModClient.java
@@ -1,14 +1,1 @@
-package com.example;
-
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-
-public class RotationModClient implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            // Your client event handling logic here
-            System.out.println("RotationModClient is starting!");
-        });
-    }
-}
+<content of RotationModClient.java>
